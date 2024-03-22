9971994d7df1f3adb365bd48b5c74ac71d4b873c
Diego Brito <diego.brito@f1rst.com.br>
2024-03-22 07:26:24 -0300

    update package

--- src/main/java/br/com/fiap/timelog/core/security/JwtTokenService.java
+++ src/main/java/br/com/fiap/timelog/core/security/JwtTokenService.java
@@ -1,4 +1,4 @@
-package br.com.fiap.pontoeletronico.core.security;
+package br.com.fiap.timelog.core.security;
 
 import com.auth0.jwt.JWT;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,7 +20,7 @@ public class JwtTokenService {
 
     private static final String ISSUER = "pos-tech-api";
 
-    public String generateToken(UsuarioDetails user) {
+    public String generateToken(UserDetailsImpl user) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
             return JWT.create()
@@ -28,6 +28,7 @@ public class JwtTokenService {
                     .withIssuedAt(creationDate())
                     .withExpiresAt(expirationDate())
                     .withSubject(user.getUsername())
+                    .withClaim("userId",user.getUserId())
                     .sign(algorithm);
         } catch (JWTCreationException exception){
             throw new JWTCreationException("Erro ao gerar token.", exception);
@@ -40,10 +41,23 @@ public class JwtTokenService {
             return JWT.require(algorithm)
                     .withIssuer(ISSUER)
                     .build()
-                    .verify(token)
+                    .verify(token.replaceFirst("Bearer ", ""))
                     .getSubject();
         } catch (JWTVerificationException exception){
             throw new JWTVerificationException("Token inválido ou expirado.");
+        }
+    }
+
+    public Long getUserIdFromToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+            return JWT.require(algorithm)
+                    .withIssuer(ISSUER)
+                    .build()
+                    .verify(token.replaceFirst("Bearer ", ""))
+                    .getClaim("userId").asLong();
+        } catch (JWTVerificationException exception){
+            throw new JWTVerificationException("Token inválido ou expirado." +  exception.getMessage());
         }
     }
 
